e71a2be0572fe36098f3cd9438b1fe2275458d54
bhagyesh <bhagyesh@bhagyeshs-mbp.bbrouter>
2023-04-21 13:44:42 +0530

    Initial Commit

---
+++ Test Suites/ExtentTestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ExtentTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>649c51e5-5537-4c91-bc24-0606dc6aa70a</testSuiteGuid>
+   <testCaseLink>
+      <guid>51291d22-cd4a-4e60-a74d-915f9b0ad99c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
